59126f41912ce1b183b2e6737362866980056970
Moksh <Your email Address>
2024-04-22 18:21:29 +0530

    hi

---
+++ recipes/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    ingredients = models.TextField()
+    instructions = models.TextField()
+    image = models.ImageField(upload_to='recipe_images/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
